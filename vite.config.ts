5615f653ca51226da8fb79c5fc9a14ceb4decba0
yashraj841 <yashraj7620@gmail.com>
2025-09-02 01:11:17 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,13 @@
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react-swc"
-import path from "path"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
 export default defineConfig({
+  base: "/job-portal-ui-Version1/", // 👈 this is the fix
   plugins: [react()],
-  base: "/job-portal-ui-Version1/", 
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
-
+});
