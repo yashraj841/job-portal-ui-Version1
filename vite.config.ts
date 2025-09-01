2bc8694164d52d6cb524e59893261c8d89f05329
yashraj841 <yashraj7620@gmail.com>
2025-09-02 00:28:53 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,11 @@ import path from "path"
 
 export default defineConfig({
   plugins: [react()],
+  base: "/job-portal-ui-Version1/", 
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
 })
+
